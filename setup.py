06df9a07f251a5813b68201c9ab1d94bcfb195c7
toddkarin <toddkarin@lbl.gov>
2019-09-09 17:39:46 +0200

    increase version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pvcz",
-    version="0.2.0",
+    version="0.2.2",
     author="toddkarin",
     author_email="pvtools.lbl@gmail.com",
     description="Photovoltaic climate zones and degradation stressors",
